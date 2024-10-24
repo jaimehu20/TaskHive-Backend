d5e57bfb030d16f175354f586b65a86875008486
Jaime Hurtado <156006941+jaimehu20@users.noreply.github.com>
2024-10-24 14:38:09 +0200

    ADD/ PreferencesSchema

--- interfaces/user.ts
+++ interfaces/user.ts
@@ -4,4 +4,42 @@ export interface User {
     birthdate: string,
     email: string,
     password: string,
+    preferences?: Preferences,
+    profile?: Profile,
+    tasks?: Task[]
+}
+
+interface Preferences {
+    reminder: boolean,
+    reminderFrequency: string,
+    dateFormat: string,
+    timeFormat: string,
+    taskPriority: string,
+    uiMode: string,
+    weekBegins: string,
+}
+
+interface Profile {
+    avatar: string,
+    bio: string,
+    ocupation: string
+}
+
+interface Task {
+    taskName: string,
+    taskDescription: string,
+    deadLine: string,
+    priority: string,
+    status: string,
+    tags: Tags[],
+    guests: Guest[]
+}
+
+interface Tags {
+    name: string
+}
+
+interface Guest {
+    name: string,
+    profilePic: string
 }
--- models/user.ts
+++ models/user.ts
@@ -1,12 +1,48 @@
 import { Schema, model } from "mongoose";
 import { User } from "../interfaces/user";
 
+const PreferencesSchema: Schema = new Schema({
+    reminder: { type: Boolean, default: false },
+    reminderFrequency: { type: String, default: '' },
+    dateFormat: { type: String, default: '' },
+    timeFormat: { type: String, default: '' },
+    uiMode: { type: String, default: 'light' },
+    weekBegins: { type: String, default: 'monday' }
+}, {
+    _id: false
+});
+
+const ProfileSchema: Schema = new Schema({
+    avatar: { type: String, default: '' },
+    bio: { type: String, default: '' },
+    ocupation: { type: String, default: '' }
+}, {
+    _id: false
+});
+
+const TaskSchema: Schema = new Schema({
+    taskName: {type: String, default: ''},
+    taskDescription: { type: String, default: ''},
+    deadLine: { type: String, default: ''},
+    priority: { type: String, default: '' },
+    status: { type: String, default: 'Pending' },
+    tags: { type: Array, default: []},
+    guests: { type: Array, default: [] }
+}, {
+    _id: false
+})
+
 const UserSchema = new Schema<User>({
     name: {type: String, required: true},
     lastname: {type: String, required: true},
     birthdate: {type: String, required: true},
     email: {type: String, required: true, unique: true},
-    password: {type: String, required: true}
-})
+    password: {type: String, required: true},
+    preferences: {type: PreferencesSchema, default: {}},
+    profile: {type: ProfileSchema, default: {}},
+    tasks: { type: [TaskSchema], default: [] }
+}, {
+    versionKey: false
+});
 
 export const UserModel = model<User>('User', UserSchema);
